b9549a70a3ec711a9b6cc690aacc6a7143e17822
Rodrigo Lazo <rlazo@users.noreply.github.com>
2024-07-15 12:52:48 -0400

    Add function calling snippets (#200)
    Co-authored-by: Daymon <17409137+daymxn@users.noreply.github.com>

--- generativeai/src/commonMain/kotlin/dev/shreyaspatil/ai/client/generativeai/type/Tool.kt
+++ generativeai/src/commonMain/kotlin/dev/shreyaspatil/ai/client/generativeai/type/Tool.kt
@@ -22,7 +22,9 @@ package dev.shreyaspatil.ai.client.generativeai.type
  * @param functionDeclarations The set of functions that this tool allows the model access to
  * @param codeExecution This is a flag value to enable Code Execution. Use [CODE_EXECUTION].
  */
-class Tool(
+class Tool
+@JvmOverloads
+constructor(
   val functionDeclarations: List<FunctionDeclaration>? = null,
   val codeExecution: JSONObject? = null,
 ) {
